248896d11a2f848941ed859b4f0a9eb00b5f02ad
David Konsumer <konsumer@jetboystudio.com>
2022-07-05 22:15:36 -0700

    define not used

--- sources/main.c
+++ sources/main.c
@@ -1,19 +1,5 @@
 #include "raylib.h"
 
-// these are the button-consts that your thing responds to
-#define A 0
-#define B 1
-#define X 2
-#define Y 3
-#define L1 4
-#define R1 5
-#define START 6
-#define SELECT 7
-#define LEFT 8
-#define RIGHT 9
-#define UP 10
-#define DOWN 11
-
 // map raylib keys to buttons
 int map_keys[] = {
   KEY_X,           // A
@@ -48,88 +34,88 @@ int map_gamepad[] = {
 
 // this is the current state of the virtual gamepad
 bool gamepad_state[] = {
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false
+  false, // A
+  false, // B
+  false, // X
+  false, // Y
+  false, // L1
+  false, // R1
+  false, // START
+  false, // SELECT
+  false, // LEFT
+  false, // RIGHT
+  false, // UP
+  false, // DOWN
 };
 
 
 int main(void) {
-    const int screenWidth = 320;
-    const int screenHeight = 240;
+  const int screenWidth = 320;
+  const int screenHeight = 240;
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
+  SetConfigFlags(FLAG_MSAA_4X_HINT);
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - gamepad mapped input");
-    Texture2D texPs3Pad = LoadTexture(ASSETS_PATH"/bg.png");
+  InitWindow(screenWidth, screenHeight, "raylib [core] example - gamepad mapped input");
+  Texture2D texPs3Pad = LoadTexture(ASSETS_PATH"/bg.png");
 
-    Texture2D gamepad_images[] = {
-        LoadTexture(ASSETS_PATH"/a.png"),
-        LoadTexture(ASSETS_PATH"/b.png"),
-        LoadTexture(ASSETS_PATH"/x.png"),
-        LoadTexture(ASSETS_PATH"/y.png"),
-        LoadTexture(ASSETS_PATH"/l1.png"),
-        LoadTexture(ASSETS_PATH"/r1.png"),
-        LoadTexture(ASSETS_PATH"/start.png"),
-        LoadTexture(ASSETS_PATH"/select.png"),
-        LoadTexture(ASSETS_PATH"/left.png"),
-        LoadTexture(ASSETS_PATH"/right.png"),
-        LoadTexture(ASSETS_PATH"/up.png"),
-        LoadTexture(ASSETS_PATH"/down.png"),
-    };
+  Texture2D gamepad_images[] = {
+    LoadTexture(ASSETS_PATH"/a.png"),
+    LoadTexture(ASSETS_PATH"/b.png"),
+    LoadTexture(ASSETS_PATH"/x.png"),
+    LoadTexture(ASSETS_PATH"/y.png"),
+    LoadTexture(ASSETS_PATH"/l1.png"),
+    LoadTexture(ASSETS_PATH"/r1.png"),
+    LoadTexture(ASSETS_PATH"/start.png"),
+    LoadTexture(ASSETS_PATH"/select.png"),
+    LoadTexture(ASSETS_PATH"/left.png"),
+    LoadTexture(ASSETS_PATH"/right.png"),
+    LoadTexture(ASSETS_PATH"/up.png"),
+    LoadTexture(ASSETS_PATH"/down.png"),
+  };
 
-    SetTargetFPS(60);
+  SetTargetFPS(60);
 
-    while (!WindowShouldClose()){
-        // this is the actual mapping
-        for (int i=0; i< sizeof(gamepad_state); i++) {
-          if (IsKeyPressed(map_keys[i])) {
-            gamepad_state[i] = true;
-          }
-          if (IsKeyReleased(map_keys[i])) {
-            gamepad_state[i] = false;
-          }
-          if (IsGamepadAvailable(0)) {
-            if (IsGamepadButtonPressed(0, map_gamepad[i])) {
-              gamepad_state[i] = true;
-            }
-            if (IsGamepadButtonReleased(0, map_gamepad[i])) {
-             gamepad_state[i] = false;
-            }
-          }
+  while (!WindowShouldClose()){
+    // this is the actual mapping
+    for (int i=0; i< sizeof(gamepad_state); i++) {
+      if (IsKeyPressed(map_keys[i])) {
+        gamepad_state[i] = true;
+      }
+      if (IsKeyReleased(map_keys[i])) {
+        gamepad_state[i] = false;
+      }
+      if (IsGamepadAvailable(0)) {
+        if (IsGamepadButtonPressed(0, map_gamepad[i])) {
+          gamepad_state[i] = true;
         }
-
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        if (IsGamepadAvailable(0)) {
-            DrawText(TextFormat("Virtual: %s", GetGamepadName(0)), 10, 10, 10, BLACK);
-        } else {
-            DrawText("Virtual Controller", 10, 10, 10, BLACK);
+        if (IsGamepadButtonReleased(0, map_gamepad[i])) {
+          gamepad_state[i] = false;
         }
-
-        DrawTexture(texPs3Pad, 0, 50, WHITE);
-
-        for (int i=0;i<sizeof(gamepad_state); i++) {
-            if (gamepad_state[i]) {
-                DrawTexture(gamepad_images[i], 0, 2, WHITE);
-            }
-        }
-        
-        EndDrawing();
+      }
     }
 
-    UnloadTexture(texPs3Pad);
-    CloseWindow();
-    return 0;
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    if (IsGamepadAvailable(0)) {
+      DrawText(TextFormat("Virtual: %s", GetGamepadName(0)), 10, 10, 10, BLACK);
+    } else {
+      DrawText("Virtual Controller", 10, 10, 10, BLACK);
+    }
+
+    DrawTexture(texPs3Pad, 0, 50, WHITE);
+
+    for (int i=0;i<sizeof(gamepad_state); i++) {
+      if (gamepad_state[i]) {
+        DrawTexture(gamepad_images[i], 0, 2, WHITE);
+      }
+    }
+    
+    EndDrawing();
+  }
+
+  UnloadTexture(texPs3Pad);
+  CloseWindow();
+  return 0;
 }
