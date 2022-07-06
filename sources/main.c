38226b106913ffd1e42434122c6ee4b8647cea60
David Konsumer <konsumer@jetboystudio.com>
2022-07-05 18:36:55 -0700

    use images for better button shapes and simpler loop

--- sources/main.c
+++ sources/main.c
@@ -64,13 +64,28 @@ bool gamepad_state[] = {
 
 
 int main(void) {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 320;
+    const int screenHeight = 240;
 
     SetConfigFlags(FLAG_MSAA_4X_HINT);
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - gamepad mapped input");
-    Texture2D texPs3Pad = LoadTexture(ASSETS_PATH"/ps3.png");
+    Texture2D texPs3Pad = LoadTexture(ASSETS_PATH"/bg.png");
+
+    Texture2D gamepad_images[] = {
+        LoadTexture(ASSETS_PATH"/a.png"),
+        LoadTexture(ASSETS_PATH"/b.png"),
+        LoadTexture(ASSETS_PATH"/x.png"),
+        LoadTexture(ASSETS_PATH"/y.png"),
+        LoadTexture(ASSETS_PATH"/l1.png"),
+        LoadTexture(ASSETS_PATH"/r1.png"),
+        LoadTexture(ASSETS_PATH"/start.png"),
+        LoadTexture(ASSETS_PATH"/select.png"),
+        LoadTexture(ASSETS_PATH"/left.png"),
+        LoadTexture(ASSETS_PATH"/right.png"),
+        LoadTexture(ASSETS_PATH"/up.png"),
+        LoadTexture(ASSETS_PATH"/down.png"),
+    };
 
     SetTargetFPS(60);
 
@@ -98,53 +113,18 @@ int main(void) {
         ClearBackground(RAYWHITE);
 
         if (IsGamepadAvailable(0)) {
-            DrawText(TextFormat("GP1: %s", GetGamepadName(0)), 10, 10, 10, BLACK);
+            DrawText(TextFormat("Virtual: %s", GetGamepadName(0)), 10, 10, 10, BLACK);
+        } else {
+            DrawText("Virtual Controller", 10, 10, 10, BLACK);
         }
-        DrawTexture(texPs3Pad, 0, 0, DARKGRAY);
 
-        // Draw buttons: basic
-        if (gamepad_state[SELECT]) DrawRectangle(328, 170, 32, 13, RED);
-        if (gamepad_state[START]) DrawTriangle((Vector2){ 436, 168 }, (Vector2){ 436, 185 }, (Vector2){ 464, 177 }, RED);
-        if (gamepad_state[X]) DrawCircle(557, 144, 13, LIME);
-        if (gamepad_state[A]) DrawCircle(586, 173, 13, RED);
-        if (gamepad_state[B]) DrawCircle(557, 203, 13, VIOLET);
-        if (gamepad_state[Y]) DrawCircle(527, 173, 13, PINK);
+        DrawTexture(texPs3Pad, 0, 50, WHITE);
 
-        // Draw buttons: d-pad
-        DrawRectangle(225, 132, 24, 84, BLACK);
-        DrawRectangle(195, 161, 84, 25, BLACK);
-        if (gamepad_state[UP]) DrawRectangle(225, 132, 24, 29, RED);
-        if (gamepad_state[DOWN]) DrawRectangle(225, 132 + 54, 24, 30, RED);
-        if (gamepad_state[LEFT]) DrawRectangle(195, 161, 30, 25, RED);
-        if (gamepad_state[RIGHT]) DrawRectangle(195 + 54, 161, 30, 25, RED);
-
-        // Draw buttons: left-right back buttons
-        if (gamepad_state[L1]) DrawCircle(239, 82, 20, RED);
-        if (gamepad_state[R1]) DrawCircle(557, 82, 20, RED);
-
-
-        // Axis & center-button is not used, all input is setup like an SNES controller (D-pad + buttons)
-
-        // // Draw buttons: ps
-        // if (IsGamepadButtonDown(0, GAMEPAD_BUTTON_MIDDLE)) DrawCircle(396, 222, 13, RED);
-
-        // // Draw axis: left joystick
-        // DrawCircle(319, 255, 35, BLACK);
-        // DrawCircle(319, 255, 31, LIGHTGRAY);
-        // DrawCircle(319 + (int)(GetGamepadAxisMovement(0, GAMEPAD_AXIS_LEFT_X) * 20),
-        //            255 + (int)(GetGamepadAxisMovement(0, GAMEPAD_AXIS_LEFT_Y) * 20), 25, BLACK);
-
-        // // Draw axis: right joystick
-        // DrawCircle(475, 255, 35, BLACK);
-        // DrawCircle(475, 255, 31, LIGHTGRAY);
-        // DrawCircle(475 + (int)(GetGamepadAxisMovement(0, GAMEPAD_AXIS_RIGHT_X) * 20),
-        //            255 + (int)(GetGamepadAxisMovement(0, GAMEPAD_AXIS_RIGHT_Y) * 20), 25, BLACK);
-
-        // // Draw axis: left-right triggers
-        // DrawRectangle(169, 48, 15, 70, GRAY);
-        // DrawRectangle(611, 48, 15, 70, GRAY);
-        // DrawRectangle(169, 48, 15, (int)(((1 - GetGamepadAxisMovement(0, GAMEPAD_AXIS_LEFT_TRIGGER)) / 2) * 70), RED);
-        // DrawRectangle(611, 48, 15, (int)(((1 - GetGamepadAxisMovement(0, GAMEPAD_AXIS_RIGHT_TRIGGER)) / 2) * 70), RED);
+        for (int i=0;i<sizeof(gamepad_state); i++) {
+            if (gamepad_state[i]) {
+                DrawTexture(gamepad_images[i], 0, 2, WHITE);
+            }
+        }
         
         EndDrawing();
     }
